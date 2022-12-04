b091cd855a6be4a845f64447f5dfe62a9368fc2b
nofutofu <aaronrweis@gmail.com>
2022-12-04 10:49:04 -0600

    added user signup and login routes

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,4 +1,50 @@
 const router = require('express').Router();
 const { User } = require('../../models');
 
-router.post('/', async (req))
+router.post('/', async (req, res) => {
+  try {
+    const userData = await User.create(req.body);
+
+    req.session.save(() => {
+      req.session.user_id = userData.id;
+      req.session.logged_in = true;
+
+      res.status(200).json(userData);
+    });
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+router.post('/login', async (req, res) => {
+  try {
+    const userData = await User.findOne({
+      where: { username: req.body.username },
+    });
+
+    if (!userData) {
+      res
+        .status(400)
+        .json({ message: 'Incorrect login info, please try again' });
+      return;
+    }
+
+    const validPass = await userData.checkPassword(req.body.password);
+
+    if (!validPass) {
+      res
+        .status(400)
+        .json({ message: 'Incorrect login info, please try again' });
+      return;
+    }
+
+    req.session.save(() => {
+      req.session.user_id = userData.id;
+      req.session.logged_in = true;
+
+      res.json({ user: userData, message: 'Login successful' });
+    });
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
