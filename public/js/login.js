a75d39427f8698fb98701540a2ab157eac669fa6
nofutofu <aaronrweis@gmail.com>
2022-12-04 11:08:08 -0600

    signup handler added

--- public/js/login.js
+++ public/js/login.js
@@ -18,3 +18,28 @@ const handleLogin = async event => {
     }
   }
 };
+
+const handleSignup = async event => {
+  event.preventDefault();
+
+  const username = document.querySelector('#username-signup').value.trim();
+  const password = document.querySelector('#password-signup').value.trim();
+
+  if (username && password) {
+    const response = await fetch('/api/users', {
+      method: 'POST',
+      body: JSON.stringify({ username, password }),
+      headers: { 'Content-Type': 'application/json' },
+    });
+
+    if (!response.ok) {
+      alert(response.statusText);
+    } else {
+      document.location.replace('/dashboard');
+    }
+  }
+};
+
+document.querySelector('.login-form').addEventListener('submit', handleLogin);
+
+document.querySelector('.signup-form').addEventListener('submit', handleSignup);
